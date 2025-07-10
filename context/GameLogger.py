0896239b68756f4177ae0931db4a1619bf30ce5b
Aslanfan <caleb.j.starkey119@gmail.com>
2025-07-10 16:49:43 -0500

    more changes

--- context/GameLogger.py
+++ context/GameLogger.py
@@ -1,34 +1,28 @@
 from typing import List, Dict, Optional
 from player_context import PlayerContext
-import game_context
-import player
+from game_context import GameContext
+from player import Player
 import json
 
 class GameLogger:
-    def __init__(self, players: List):
+    player_list: List[Player]
+    def __init__(self, players: List[Players]):
+        self.player_list = players
         self.log = {
-                "rounds": [], 
-                    "players": [
-                {
-                    "playerId": p.player_id,
-                    "name": p.name,
-                    "color": p.color
-                } for p in players
-            ]}
+            "rounds": [], 
+            "players": [{
+                "playerId": p.player_id,
+                "name": p.name,
+                "color": p.color
+            } for p in players
+        ]}
 
     def add_round(context:PlayerContext, players: List, self):
         self.log["rounds"].append({
-            "players": [
-                {
-                    "playerId": p.player_id,
-                    "name": p.name,
-                    "color": p.color
-                } for p in players
-            ],
             "turns": []
         })
 
-    def add_turn(roundNumber:int, context:PlayerContext, self):
+    def add_turn(round_number:int, context:PlayerContext, self):
         """
         Export a PlayerContext + full player list to JSON format.
         Assumes each player object has:
@@ -36,122 +30,86 @@ class GameLogger:
           - .get_claimed_routes(), .get_exposed(), .get_card_count(), .get_hidden_card_count()
           - .get_tickets() returning .start, .end, .points, .completed
         """
-        player_data = []
-        opponents_data = {}
-        claimed_routes = []
+        player_data = {}
+        opponents_data = []
         longest_path = context.map.get_longest_path()
         
         
-        for player in self.log["rounds"]["players"]:
-            player_data.append({
-                "playerId": context.player_id,
-                "score": context.score,
-                "trainCarCounts": player.trains_remaining,
-                "claimedRoutes": player.get_claimed_routes(),
-                "destinationTickets": [
-                    {
-                        "from": t.start,
-                        "to": t.end,
-                        "points": t.points,
-                        "completed": t.completed
-                    } for t in player.get_tickets()
-                ],
-                "hand": {
-                    "public": dict(player.get_exposed()),
-                    "hidden": player.get_hidden_card_count()
-                }
-            })
-
-            for route in player.get_claimed_routes():
-                claimed_routes.append({
-                    "routeID": route,
-                    "claimedBy": player.player_id
+        for player in self.log["players"]:
+            if player["player_id"] == context.player_id:
+                player_data = ({
+                    "player_id": context.playerId,
+                    "score": context.player,
+                    "remaining_trains": context.remaining_trians,
+                    "claimed_routes": [route for route in context.map.routes if route.claimed_by is context.player_id],
+                    "longest_path": context.longest_path,
+                    "has_longest_path": context.has_longest_path, 
+                    "destinationTickets": [
+                        {
+                            "from": ,
+                            "to": "Raleigh",
+                            "points": ,
+                            "completed":  
+                        },
+                        {
+                            "from": ,
+                            "to": ,
+                            "points": ,
+                            "completed": 
+                        }
+                    ],
+                    "hand": { 
+                        "black": ,
+                        "blue": ,
+                        "green": ,
+                        "locomotive": ,
+                        "orange": ,
+                        "purple": ,
+                        "red": ,
+                        "white": ,
+                        "yellow": 
+                    }
+                })
+            else:
+                opponents_data.append({
+                    "playerId": context.player_id,
+                    "score": context.score,
+                    "trainCarCount": player.trains_remaining,
+                    "claimed_routes": [route for route in context.map.routes if route.claimed_by is playerId],
+                    "longest_path": context.longest_path,
+                    "destinationTickets": [
+                        {
+                            "from": t.start,
+                            "to": t.end,
+                            "points": t.points,
+                            "completed": t.completed
+                        } for t in player.get_tickets()
+                    ],
+                    "hand": {
+                        "public": dict(player.get_exposed()),
+                        "hidden": player.get_card_count() - player.get_exposed().length()
+                    }
                 })
 
         turn_state = {
-            "gameObjects": {
+            "player": player_data,
+            "opponents": opponents_data,
+            "game_objects": {
                 "map": {
-                    "claimedRoutes": claimed_routes,
-                    "longestPath": longest_path
+                    "claimed_routes": claimed_routes,
+                    "longest_path": [
+                        "length": map.get_longest_path(player_list)["length"],
+                        "player_id": map.get_longest_path(player_list)[""]
+                    ]
                 },
                 "decks": {
-                    "trainDeckCount": context.train_deck.remaining(),
-                    "destinationDeckCount": context.ticket_deck.remaining(),
-                    "faceUpTrainCards": context.face_up_cards
+                    "train_deck_count": context.train_deck.remaining(),
+                    "destination_deck_count": context.ticket_deck.remaining(),
+                    "face_up_train_cards": context.face_up_cards
                 }
-            },
-            "playerInfo": player_data,
-            "opponentInfo": opponents_data
+            }
         }
-        self.log["rounds"]["turns"].append(turn_state)
-
-    def add_round_to_json(context: PlayerContext, players: List) -> dict:
-        """
-        Export a PlayerContext + full player list to JSON format.
-        Assumes each player object has:
-          - .player_id, .name, .color, .get_score(), .trains_remaining
-          - .get_claimed_routes(), .get_exposed(), .get_card_count(), .get_hidden_card_count()
-          - .get_tickets() returning .start, .end, .points, .completed
-        """
-        players_data = []
-        claimed_routes = []
-        longest_path = context.map.get_longest_path_players()  # e.g., ["p1"]
-
-        for player in players:
-            players_data.append({
-                "playerId": player.player_id,
-                "score": player.get_score(),
-                "trainCarCounts": player.trains_remaining,
-                "claimedRoutes": player.get_claimed_routes(),
-                "destinationTickets": [
-                    {
-                        "from": t.start,
-                        "to": t.end,
-                        "points": t.points,
-                        "completed": t.completed
-                    } for t in player.get_tickets()
-                ],
-                "hand": {
-                    "public": dict(player.get_exposed()),
-                    "hidden": player.get_hidden_card_count()
-                }
-            })
-
-            for route in player.get_claimed_routes():
-                claimed_routes.append({
-                    "routeID": route,
-                    "claimedBy": player.player_id
-                })
-
-        turn_state = {
-            "turnNumber": context.turn_number,
-            "gameObjects": {
-                "map": {
-                    "claimedRoutes": claimed_routes,
-                    "longestPath": longest_path
-                },
-                "decks": {
-                    "trainDeckCount": context.train_deck.remaining(),
-                    "destinationDeckCount": context.ticket_deck.remaining(),
-                    "faceUpTrainCards": context.face_up_cards
-                }
-            },
-            "playerInfo": players_data
-        }
-
-        game_state = {
-            "roundID": context.context.get_round_id(),
-            "players": [
-                {
-                    "playerId": p.player_id,
-                    "name": p.name,
-                    "color": p.color
-                } for p in players
-            ],
-            "turns": [turn_state]
-        }
-
-        return game_state
+        self.log["rounds"][round_number]["turns"].append(turn_state)
     
     def log_turn_for_all_players(self, context, players):
         for player in players:
--- context/player_context.py
+++ context/player_context.py
@@ -11,6 +11,8 @@ class OpponentInfo:
     exposed_hand: Counter[str]
     num_cards_in_hand: int
     remaining_trains: int
+    longest_path: int
+    has_longest_path: bool
     score: int
 
 class PlayerContext:
@@ -18,10 +20,12 @@ class PlayerContext:
         self.player_id = player_id
         self.map = deepcopy(context.get_map())
         self.train_deck = deepcopy(context.get_train_deck())
-        self.ticket_deck = deepcopy(context.get_ticket_deck())
         self.face_up_cards = face_up_cards = context.get_train_deck().face_up()
         self.available_routes = context.get_map().get_available_routes()
-        self.ticket_deck = context.get_ticket_deck()
+        self.longest_path = map.get_longest_path(player_id)
+        self.has_longest_path = map.get_longest_path(context.player)
+        self.ticket_deck = context.get_ticket_deck(players)
+        self.destination_tickets = 
         self.turn_number = context.turn_num
         self.score = context.get_score(player_id)
 
@@ -32,6 +36,8 @@ class PlayerContext:
                 exposed_hand = p.get_exposed(),
                 num_cards_in_hand = p.get_card_count(),
                 remaining_trains = p.trains_remaining,
+                longest_path = map.get_longest_path(player_id),
+                has_longest_path = map.get_longest_path(context.player),
                 score = context.get_score(p.player_id)
             ) for p in players if p.player_id != self.player_id
         ]
