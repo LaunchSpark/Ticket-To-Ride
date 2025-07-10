c0c81a78e6eaf29ec9864101073a1659cb09a1aa
Aslanfan <caleb.j.starkey119@gmail.com>
2025-07-10 13:37:50 -0500

    added score to player_context and removed context from attributes

--- context/player_context.py
+++ context/player_context.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from collections import Counter
 import weakref
 from copy import deepcopy
+from game_context import GameContext
 
 @dataclass
 class OpponentInfo:
@@ -10,25 +11,27 @@ class OpponentInfo:
     exposed_hand: Counter[str]
     num_cards_in_hand: int
     remaining_trains: int
+    score: int
 
 class PlayerContext:
-    def __init__(self,player_id,context,players):
+    def __init__(self,player_id: str,context: GameContext, players: List):
         self.player_id = player_id
         self.map = deepcopy(context.get_map())
         self.train_deck = deepcopy(context.context.get_train_deck())
         self.ticket_deck = deepcopy(context.context.get_ticket_deck())
-        self.context = context
         self.face_up_cards = face_up_cards = self.context.get_train_deck().face_up()
         self.available_routes = self.context.get_map().get_available_routes()
         self.ticket_deck = self.context.get_ticket_deck()
         self.turn_number = context.turn_num
+        self.score = context.get_score(player_id)
 
         self.opponents = [
             OpponentInfo(
                 player_id = p.player_id,
                 exposed_hand = p.get_exposed(),
                 num_cards_in_hand = p.get_card_count(),
-                remaining_trains = p.trains_remaining
+                remaining_trains = p.trains_remaining,
+                score = context.get_score(p.player_id)
             ) for p in players if p.player_id != self.player_id
         ]
 
